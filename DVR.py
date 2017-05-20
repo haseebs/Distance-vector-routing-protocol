07e04bdaf83dfbd93b99a800a126594f92926bfe
Haseeb Shah <haseebs@outlook.com>
2017-05-20 21:39:28 +0500

    Implemented UDP server on separate thread

--- DVR.py
+++ DVR.py
@@ -1,21 +1,20 @@
 import sys
 import socket
 import math
+import threading
 import distanceVec_pb2
 
-#Code for reading from file in progress
-#if(len(sys.argv) < 4):
-#    print("Usage: python DVR.py <router-id> <port-no> <router-config-file>")
-#    exit()
-
-#rid = sys.argv[1]
-#pno = sys.argv[2]
-#rcf = open(sys.argv[3])
-
-#n = int(rcf.readline().strip())
-#neighbours = []
-#for nbr in range(n-1):
-    #read info about neighbours and store in 2d array
+#UDP server running on separate thread
+class server (threading.Thread):
+    def __init__(self, PORT):
+        threading.Thread.__init__(self)
+        self.port = PORT
+    def run(self):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock.bind(('', self.port))
+        while 1:
+            self.message, self.adr = self.sock.recvfrom(4096)
+            print(self.message)
 
 #Function for feeding data into protobuf generated class from file
 def readInput(vec):
@@ -25,27 +24,44 @@ def readInput(vec):
         vec.cost = float(split[1])
         vec.port = int(split[2])
 
-#for sending UDP packets
+#Function for sending UDP packets
 def sendDV(MESSAGE):
     IP = 'localhost'
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     for vec in MESSAGE.neighbours:
         PORT = vec.port
         sock.sendto(MESSAGE.SerializeToString(), (IP, PORT))
-    #sock.sendto(str(MESSAGE).encode('utf-8'), (IP, PORT))
+    sock.close()
+
+#################################
+# MAIN THREAD
+#################################
+
+#Code for reading from file (in progress)
+if(len(sys.argv) < 4):
+    print("Usage: python DVR.py <router-id> <port-no> <router-config-file>")
+    exit()
+
+routerID = sys.argv[1]
+port = int(sys.argv[2])
+#configFile = open(sys.argv[3])
+#n = int(configFile.readline().strip())
 
 print("Enter number of neighbours: ")
 n = int(input())
-print(n)
 
 distanceVec = distanceVec_pb2.Vector()
 
+#Store the inputs in protobuf ds
 for i in range(n):
     readInput(distanceVec.neighbours.add())
 
-#sendDV('localhost', 5000, float('inf'))
-sendDV(distanceVec)
+#Start UDP server thread
+serv = server(port)
+serv.start()
 
+#Send distance vector to all neighbours
+sendDV(distanceVec)
 
 #print(distanceVec)
 #print(distanceVec.SerializeToString())
